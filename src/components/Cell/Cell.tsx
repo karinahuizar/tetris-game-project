7f854850c6431ed547c65e04287b6c936173cc26
Karina Huizar <karinahuizar@gmail.com>
2022-08-26 10:53:33 -0500

    Created Cell component.

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -5,3 +5,7 @@ import { TETROMINOS } from "../../setup";
 type Props = {
     type: keyof typeof TETROMINOS;
 };
+
+const Cell: React.FC<Props> = ({ type }) => <StyledCell type={type} color={TETROMINOS[type].color} />;
+
+export default React.memo(Cell);
